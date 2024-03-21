d16105ea4be9273bddd05de47c209c2fd915826b
Bahoyvs <65951986+Bahoyvs@users.noreply.github.com>
2024-03-21 03:15:53 +0300

    MK2

---
+++ Mk 2/clientreceiver.py
@@ -0,0 +1,13 @@
+import socket
+
+HOST = '139.179.235.79'        # IP address of the server
+PORT = 65432              # Port number of the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))  # Connect to the server
+    while True:
+        data = s.recv(1024)  # Receive data from the server
+        if not data:
+            print("Server connection closed.")
+            break
+        print(data.decode())  # Print the received data
